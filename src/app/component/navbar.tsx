9e83baeb0bc4db71dd8a48162b940b74258ac40c
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2024-12-08 16:15:56 +0500

    edit styling properties

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -1,10 +1,9 @@
-
 'use client'
 import React, { useState } from "react";
 import Image from "next/image";
 
-import navbarbadge from "@/app/images/navbarbadge.png"
-import headerimage from "@/app/images/headerimage.png"
+import navbarbadge from "@/app/images/navbarbadge.png";
+import headerimage from "@/app/images/headerimage.png";
 import loginimage from "@/app/images/loginimage.png";
 import addedproduct from "@/app/images/addedproduct.png";
 import addtocart from "@/app/images/addtocart.png";
@@ -22,9 +21,10 @@ const Nav = () => {
     };
 
     return (
-        <div className=" w-[1440px] h-[60px] md:h-[136px] ">
+        <div className="w-full h-[60px] md:h-[136px]">
 
-<div className="  w-[1440px] h-[58px]  hidden md:block">
+            {/* Header Image (hidden on small screens) */}
+            <div className="w-full h-[58px] hidden lg:block">
                 <Image
                     src={headerimage}
                     alt="header image"
@@ -33,14 +33,11 @@ const Nav = () => {
                 />
             </div>
 
-
-
             {/* Navbar */}
-            <div className=" w-[1042px] h-[58px] mx-auto flex">
+            <div className="w-full md:w-[1042px] h-[58px] mx-auto flex justify-between items-center px-5">
 
-
-                {/* badge image */}
-                <div className="  w-[187px] h-[58px]">
+                {/* Badge image */}
+                <div className="w-[187px] h-[58px]">
                     <Image
                         src={navbarbadge}
                         alt="NAV BAR image"
@@ -49,74 +46,68 @@ const Nav = () => {
                     />
                 </div>
 
+                {/* Centered Links Container */}
+                <div className="flex flex-grow justify-center">
+                    {/* Navigation links (hidden on mobile by default) */}
+                    <div className={`flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 ${isMenuOpen ? "block w-full absolute top-[58px] left-0 bg-white px-4 py-4" : "hidden md:flex"} items-center justify-center`}>
+                        <Link href="/" className="text-center">Home</Link>
+                        <Link href="/" className="text-center">Shop</Link>
+                        <Link href="/" className="text-center">About</Link>
+                        <Link href="/" className="text-center">Blog</Link>
+                        <Link href="/" className="text-center">Contact</Link>
+                        <Link href="/" className="text-center">Pages</Link>
 
-
-
-                <div className=" w-[815px] h-[58px] mx-auto flex justify-between items-center px-5">
-                    {/* Navbar Links & Icons */}
-                    <div className="flex items-center justify-between w-full">
-
-                        {/* Navigation links */}
-                        <div className={`flex items-center space-x-6 md:space-x-8 ${isMenuOpen ? "flex-col absolute top-[58px] bg-white w-[414px] left-20 px-4 py-4" : "hidden md:flex"}`}>
-                            <Link href="/">Home</Link>
-                            <Link href="/">Shop</Link>
-                            <Link href="/">About</Link>
-                            <Link href="/">Blog</Link>
-                            <Link href="/">Contact</Link>
-                            <Link href="/">Pages</Link>
-
-                            {/* Login, Search & Cart Icons inside hamburger */}
-                            <div className="flex items-center space-x-4 flex-col md:flex-row space-y-4">
-                                <div className="w-[166px] h-[54px]">
-                                    <Link href="/">
-                                        <Image
-                                            src={loginimage}
-                                            alt="Login"
-                                            width={166}
-                                            height={54}
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="w-[46px] h-[46px]">
-                                    <Link href="/">
-                                        <Image
-                                            src={searchicon}
-                                            alt="Search"
-                                            width={46}
-                                            height={46}
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="w-[56px] h-[46px]">
-                                    <Link href="/">
-                                        <Image
-                                            src={addtocart}
-                                            alt="Add to cart"
-                                            width={46}
-                                            height={46}
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="w-[56px] h-[46px]">
-                                    <Link href="/">
-                                        <Image
-                                            src={addedproduct}
-                                            alt="Added product"
-                                            width={46}
-                                            height={46}
-                                        />
-                                    </Link>
-                                </div>
+                        {/* Login, Search & Cart Icons */}
+                        <div className="flex items-center space-x-4">
+                            <div className="w-[166px] h-[54px]">
+                                <Link href="/">
+                                    <Image
+                                        src={loginimage}
+                                        alt="Login"
+                                        width={166}
+                                        height={54}
+                                    />
+                                </Link>
+                            </div>
+                            <div className="w-[46px] h-[46px]">
+                                <Link href="/">
+                                    <Image
+                                        src={searchicon}
+                                        alt="Search"
+                                        width={46}
+                                        height={46}
+                                    />
+                                </Link>
+                            </div>
+                            <div className="w-[56px] h-[46px]">
+                                <Link href="/">
+                                    <Image
+                                        src={addtocart}
+                                        alt="Add to cart"
+                                        width={46}
+                                        height={46}
+                                    />
+                                </Link>
+                            </div>
+                            <div className="w-[56px] h-[46px]">
+                                <Link href="/">
+                                    <Image
+                                        src={addedproduct}
+                                        alt="Added product"
+                                        width={46}
+                                        height={46}
+                                    />
+                                </Link>
                             </div>
                         </div>
-
-                        {/* Hamburger menu for small screens */}
-                        <div className=" md:hidden flex items- cursor-pointer" onClick={toggleMenu}>
-                            <span className="text-xl">{isMenuOpen ? "X" : <i className="fa-solid fa-bars"></i>}</span> {/* Hamburger icon or close button */}
-
-                        </div>
-
                     </div>
+                </div>
+
+                {/* Hamburger menu for small screens (on the right side) */}
+                <div className="md:hidden flex items-center cursor-pointer" onClick={toggleMenu}>
+                    <span className="text-xl">
+                        {isMenuOpen ? "X" : <FontAwesomeIcon icon={faBars} />}
+                    </span>
                 </div>
             </div>
         </div>
@@ -124,4 +115,3 @@ const Nav = () => {
 }
 
 export default Nav;
-
